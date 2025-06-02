da5a1dc63fb94af31d00d6ac0aacd1e9ae8e3a2e
Artiom Bezkorovainyi <s30174@pjwstk.edu.pl>
2025-06-02 20:31:44 +0200

    add

---
+++ pja-apbd-cwic12/Controllers/TripsController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using pja_apbd_cwic12.DTOs;
+using pja_apbd_cwic12.Exceptions;
+using pja_apbd_cwic12.Services;
+
+namespace pja_apbd_cwic12.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TripsController(IDbService service) : ControllerBase
+{
+    [HttpGet("{page?}")]
+    public async Task<IActionResult> GetTripsAsync(int page = 0, int paginate = 10)
+    {
+        return Ok(await service.GetAllTripsAsync(page, paginate));
+    }
+
+    [HttpPost("{idTrip}/clients")]
+    public async Task<IActionResult> PutClientOnATripAsync(int idTrip, ClientPostDTO client)
+    {
+        try
+        {
+            await service.AssignClientToTripAsync(idTrip, client);
+            return Created();
+        }
+        catch (ClientExistsException _)
+        {
+            return BadRequest("Client exists");
+        }
+        catch (TripAlreadyStartedException _)
+        {
+            return BadRequest("Trip has already started");
+        }
+        catch (InvalidOperationException _)
+        {
+            return NotFound("No such trip");
+        }
+    }
+}
